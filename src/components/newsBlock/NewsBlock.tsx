ff218998eb65b774a08c247a58249ebac3d70c80
Evgeny Loy <evgenyloy@email.com>
2026-05-13 13:45:02 +0300

    The site navigation has been changed from the Browse all button.

--- src/components/newsBlock/NewsBlock.tsx
+++ src/components/newsBlock/NewsBlock.tsx
@@ -6,22 +6,13 @@ import { newsStore } from "../../store/newsStore";
 import { useGetNewsList } from "../../hooks/gamesQueries";
 import NewsView from "./NewsView";
 import { onNewsLoaded } from "./utils";
+import { Link } from "react-router-dom";
 import "./newsBlock.scss";
 
 function NewsBlock() {
   const { data: news = [], isPending, isError, isSuccess } = useGetNewsList();
   const setNews = newsStore.use.setNews();
-  let newsList = useRef<null | HTMLDivElement>(null);
   const nodeRef = useRef(null);
-
-  const handleButtonClick = useCallback(() => {
-    if (newsList.current === null) return;
-    newsList.current.scrollIntoView({ block: "start", behavior: "smooth" });
-  }, []);
-
-  useEffect(() => {
-    newsList.current = document.querySelector(".news-list");
-  }, [handleButtonClick]);
 
   const content = onNewsLoaded(news);
 
@@ -36,7 +27,7 @@ function NewsBlock() {
         <div className="news__top">
           <h2 className="news__info-title">Last news</h2>
           <div className="news__button button">
-            <button onClick={() => handleButtonClick()}>browse all</button>
+            <Link to={"/news-list"}>browse all</Link>
           </div>
         </div>
 
